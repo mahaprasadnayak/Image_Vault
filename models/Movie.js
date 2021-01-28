c68c6bc621324a6087611416ca0b5ca2927311a8
Mahaprasad Nayak <78096612+mahaprasadnayak@users.noreply.github.com>
2021-01-28 16:45:02 +0530

    Add files via upload

---
+++ models/Movie.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    
+    img: {
+        type: Buffer,
+        required: true
+    },
+    imgType: {
+        type: String,
+        required: true
+    }
+    
+});
+
+movieSchema.virtual('coverImagePath').get(function (){
+    if(this.img != null && this.imgType != null){
+        return `data:${this.imgType};charset=utf-8;base64,${this.img.toString('base64')}`;
+    }
+})
+
+
+
+module.exports = mongoose.model('movies', movieSchema);
